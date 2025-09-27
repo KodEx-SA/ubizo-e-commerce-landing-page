1c5ee69f58638dfd17c30f3f68ccd2f2ea5a2208
Ashley K Motsie <motsieashley31@gmail.com>
2025-09-27 08:18:06 +0200

    app update

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from chat import get_response
+import os
 
 app = Flask(__name__)
 CORS(app)  # enable CORS for all routes
@@ -36,4 +37,5 @@ def predict():
 
 # deploy using vercel
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
